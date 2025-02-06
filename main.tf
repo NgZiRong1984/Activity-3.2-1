a9e5f6f29999f35f6ece322853ce0c4555fdde23
NgZiRong1984 <ngzirong1984@gmail.com>
2025-02-06 23:12:54 +0800

    checkov

--- main.tf
+++ main.tf
@@ -28,12 +28,12 @@ locals {
 
 resource "aws_s3_bucket" "s3_tf" {
   # checkov:skip=CKV2_AWS_62
-# checkov:skip=CKV_AWS_21
-# checkov:skip=CKV2_AWS_6
-# checkov:skip=CKV_AWS_18
-# checkov:skip=CKV_AWS_144
-# checkov:skip=CKV_AWS_145
-# checkov:skip=CKV2_AWS_61
+  # checkov:skip=CKV_AWS_21
+  # checkov:skip=CKV2_AWS_6
+  # checkov:skip=CKV_AWS_18
+  # checkov:skip=CKV_AWS_144
+  # checkov:skip=CKV_AWS_145
+  # checkov:skip=CKV2_AWS_61
   bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
 }
 
